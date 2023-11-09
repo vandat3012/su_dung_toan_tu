5e00a1e100e3465088966df5609f7e998d22834b
vandat3012 <luongvandat301220@gmail.com>
2023-11-09 17:07:40 +0700

    update bai tap

---
+++ su_dung_toan_tu.js
@@ -0,0 +1,6 @@
+inputWith= prompt("Enter the width")
+inputHeight= prompt("Enter the height")
+let width=parseInt(inputWith);
+let height=parseInt(inputHeight);
+let area= width*height;
+document.write('area ='+area);
